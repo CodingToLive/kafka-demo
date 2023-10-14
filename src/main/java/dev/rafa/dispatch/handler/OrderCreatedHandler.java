47075aeaea20587237267029b2bf9bc0696ef50d
rafae <rafael.ramirez@engineer.com>
2023-10-14 12:42:34 -0500

    Change configuration to Java Based

--- src/main/java/dev/rafa/dispatch/handler/OrderCreatedHandler.java
+++ src/main/java/dev/rafa/dispatch/handler/OrderCreatedHandler.java
@@ -18,7 +18,8 @@ public class OrderCreatedHandler {
     @KafkaListener(
             id = "orderConsumerClient",
             topics = "order.created",
-            groupId = "dispatch.order.created.consumer"
+            groupId = "dispatch.order.created.consumer",
+            containerFactory = "kafkaListenerContainerFactory"
     )
     public void listen(OrderCreated payload) {
         log.info("Received Message: payload: " + payload);
